1af9d33f312c80f811c178060231be49d4e89f8a
John Doe <johndoe@example.com>
2015-01-22 23:45:22 -0300

    commit conflit

--- src/test/Test.java
+++ src/test/Test.java
@@ -17,6 +17,7 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("Hello" + args[0]);
         System.out.println("Hello" + args[0]);
+        System.out.println("conflit") ;
     }
     
 }
